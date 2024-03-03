b8317a48bbf1228dfccf54b7de74dd68a792f77b
FLoWIsMe <daelynd1@gmail.com>
2024-03-03 13:22:22 -0500

    Updated file structure to accomodate porting of the model

---
+++ Compressed Models/PrunedModel.py
@@ -0,0 +1,23 @@
+# Loading the trained model
+import torch
+import torch.nn as nn
+from ControlModel import FeedforwardNeuralNetModel
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+import torch.nn.utils.prune as prune
+import torch.nn.functional as F
+
+
+# Assuming FeedforwardNeuralNetModel class is defined here
+
+# Initialize the model
+model = FeedforwardNeuralNetModel(input_dim, hidden_dim, output_dim)
+
+# Load the model state
+model_load_path = 'FeedforwardNeuralNetModel.pth'
+model.load_state_dict(torch.load(model_load_path))
+
+# Set the model to evaluation mode if you are not training it further
+model.eval()
+
+# Now, you can use model for inference or further processing
